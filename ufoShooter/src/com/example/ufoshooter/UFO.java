4b2b8c91d2929b922bb8caf8c8f7b13e72a1d743
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-04 16:16:09 +0900

    ufo種類更新

--- ufoShooter/src/com/example/ufoshooter/UFO.java
+++ ufoShooter/src/com/example/ufoshooter/UFO.java
@@ -1,19 +1,50 @@
 package com.example.ufoshooter;
 
+import java.awt.*;
+
 public class UFO {
     public int x, y;
+    private UFOType type;
 
-    public UFO(int x, int y) {
+    public UFO(int x, int y, UFOType type) {
         this.x = x;
         this.y = y;
+        this.type = type;
     }
 
     public void move(int level) {
-        y += 1 + level; // 基礎速度 + 等級加成
+        switch (type) {
+            case NORMAL -> y += 1 + level;
+            case FAST -> y += 2 + level;
+            case TANK -> y += 0.5 + level;
+        }
     }
 
     public boolean isOutOfScreen() {
         return y > 400;
     }
-}
 
+    public UFOType getType() {
+        return type;
+    }
+
+    public void draw(Graphics2D g2d) {
+        Color color = switch (type) {
+            case NORMAL -> Color.LIGHT_GRAY;
+            case FAST -> Color.RED;
+            case TANK -> Color.GREEN;
+        };
+        g2d.setColor(color);
+        g2d.fillOval(x, y, 40, 20);
+        g2d.setColor(Color.GRAY);
+        g2d.drawOval(x, y, 40, 20);
+    }
+
+    public int getScore() {
+        return switch (type) {
+            case NORMAL -> 10;
+            case FAST -> 20;
+            case TANK -> 30;
+        };
+    }
+}
---
+++ ufoShooter/src/com/example/ufoshooter/UFOType.java
@@ -0,0 +1,5 @@
+package com.example.ufoshooter;
+
+public enum UFOType {
+    NORMAL, FAST, TANK
+}
